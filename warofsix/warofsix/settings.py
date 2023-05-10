ef6ce5b4524f97bb39c58aee347fde564d29b1b1
hamdiozdemir <hamdiozdemir61@gmail.com>
2023-05-10 21:07:12 +0300

    domain host update

--- warofsix/warofsix/settings.py
+++ warofsix/warofsix/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','localhost','161.35.209.41']
+ALLOWED_HOSTS = ['127.0.0.1','localhost','161.35.209.41', 'warofsix.com']
 
 
 # Application definition
